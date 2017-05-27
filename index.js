9632b7ed1b8698a33ebed1e3664eed2c784f4efd
Jonathan Stoler <jonstoler@users.noreply.github.com>
2017-05-26 19:13:28 -0700

    user management, character creation dialog

--- index.js
+++ index.js
@@ -5,9 +5,116 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + "/public"));
 
+process.on("SIGINT", function(){
+	io.sockets.emit("kick");
+	process.exit();
+});
+
+var gamestate = {
+	reset: function(){
+		this.characters = [];
+		this.log = [];
+		this.aspects = [];
+		this.players = [];
+		this.gm = {};
+
+		this._sockmap = {};
+	}
+};
+gamestate.reset();
+
 io.on("connection", function(socket){
-	socket.on("roll", function(data){
-		socket.broadcast.emit("roll", data);
+	socket.propagate = function(id, data){
+		socket.emit(id, data);
+		socket.broadcast.emit(id, data);
+	}
+
+	socket.on("sync_all", function(data){
+		socket.emit("sync_all", gamestate);
+	});
+	socket.on("sync_players", function(data){
+		socket.emit("sync_players", gamestate.players);
+	});
+
+	socket.on("reset", function(data){
+		gamestate.reset();
+		socket.propagate("kick");
+	});
+
+
+	socket.on("gm_exists", function(data){
+		socket.emit("gm_exists", gamestate.gm.id != undefined);
+	});
+
+	socket.on("gm_get", function(data){
+		socket.emit("gm_get", gamestate.gm);
+	});
+
+	socket.on("gm_join", function(data){
+		gamestate.gm = data;
+		data.gm = true;
+		gamestate.players.push(data);
+		gamestate._sockmap[data.id] = socket.conn.id;
+		socket.broadcast.emit("sync_players", gamestate.players);
+	});
+
+
+	socket.on("pc_join", function(data){
+		data.status = "online";
+		data.gm = false;
+		gamestate.players.push(data);
+		gamestate._sockmap[data.id] = socket.conn.id;
+		socket.broadcast.emit("sync_players", gamestate.players);
+	});
+
+	socket.on("pc_kick", function(data){
+		for(var i = 0; i < gamestate.players.length; i++){
+			if(gamestate.players[i].id == data.id){
+				if(data.id == gamestate.gm.id){
+					gamestate.gm = {};
+				}
+				gamestate.players.splice(i, 1);
+				break;
+			}
+		}
+		var sockid = gamestate._sockmap[data.id];
+		if(io.sockets.connected[sockid]){
+			io.sockets.connected[sockid].emit("kick");
+			io.sockets.connected[sockid].disconnect();
+		}
+		socket.broadcast.emit("sync_players", gamestate.players);
+	});
+
+
+	socket.on("char_new", function(data){
+		gamestate.characters.push(data);
+		socket.broadcast.emit("sync_characters", gamestate.characters);
+	});
+
+	
+	socket.on("disconnect", function(reason){
+		if(reason == "transport close"){
+			var playerid;
+			for(playerid in gamestate._sockmap){
+				if(gamestate._sockmap.hasOwnProperty(playerid)){
+					var sockid = gamestate._sockmap[playerid];			
+					if(sockid == socket.id){
+						delete gamestate._sockmap[playerid];
+						break;
+					}
+				}
+			}
+			for(var i = 0; i < gamestate.players.length; i++){
+				if(gamestate.players[i].id == playerid){
+					if(playerid == gamestate.gm.id){
+						gamestate.gm = {};
+					}
+					gamestate.players.splice(i, 1);
+					break;
+				}
+			}
+			socket.broadcast.emit("sync_players", gamestate.players);
+		}
 	});
 });
 
--- public/js/fae2fae.js
+++ public/js/fae2fae.js
@@ -1,31 +1,170 @@
 (function(){
-	var rolls = new Vue({
-		el: "#roll",
-		data: {
-			rolls: [],
-			name: "",
+
+	function generateID(){
+		var id = "";
+		for(var i = 0; i < 16; i++){
+			var ch = Math.random() * 16 | 0;
+			id += ch.toString(16);
 		}
-	});
+		return id;
+	}
+
+	var toastTimeout;
+	function toast(txt){
+		var toast = document.getElementById("toast");
+		clearTimeout(toastTimeout);
+		toast.innerText = txt;
+		toast.style.opacity = 1;
+		toastTimeout = setTimeout(function(){
+			toast.style.opacity = 0;
+		}, 3000);
+	}
+
+	var fae = new Vue({
+		el: "#play",
+		data: {
+			characters: [],
+			log: [],
+			aspects: [],
+			players: [],
+			gm: false,
+			id: generateID(),
+		},
+		computed: {
+			sortedPlayers: function(){
+				var sorted = [];
+				for(var i = 0; i < this.players.length; i++){
+					var p = this.players[i];
+					if(p.id == this.id){
+						sorted.unshift(p);
+					} else {
+						sorted.push(p);
+					}
+				}
+				return sorted;
+			},
+			sortedCharacters: function(){
+				return this.characters;
+			}
+		}
+	})
 
 	var socket = io("http://localhost:3000");
 	socket.on("connect", function(){
-		socket.on("roll", function(data){
-			rolls.rolls.push({name: data.name, roll: data.roll});
+		var gmCheck = setInterval(function(){
+			socket.emit("gm_exists");
+		}, 1000);
+		socket.emit("gm_exists");
+
+		socket.on("gm_exists", function(data){
+			var search = $("#gm-search");
+			var searchtxt = $("#gm-search-txt");
+			var found = $("#gm-found");
+			var notfound = $("#gm-not-found");
+			if(data === true){
+				found.removeClass("hidden");
+				notfound.addClass("hidden");
+				search.addClass("hidden");
+				socket.emit("gm_get");
+			} else {
+				notfound.removeClass("hidden");
+				search.removeClass("hidden");
+				found.addClass("hidden");
+				$("#gm-id").text("Someone");
+			}
+			searchtxt.addClass("hidden");
 		});
 
-		document.getElementById("roller").onclick = function(){
-			var total = 0;
-			var mod = document.getElementById("modifier");
-			for(var i = 0; i < 4; i++){
-				var r = Math.floor(Math.random() * 3) - 1;
-				total += r;
-			}
-			var add = parseInt(mod.options[mod.selectedIndex].value);
-			if(isNaN(add)){ add = 0; }
-			total += add;
+		$("#gm-join").on("click", function(){
+			var name = $("#player-name").val();
+			if(name == ""){
+				toast("Please enter a name.");
+				$("#player-name").addClass("error");
+				$("#player-name").focus();
+			} else {
+				socket.emit("gm_join", {
+					name: $("#player-name").val(),
+					id: fae.id,
+				});
+				clearInterval(gmCheck);
+				fae.gm = true;
 
-			rolls.rolls.push({name: rolls.name, roll: total});
-			socket.emit("roll", {name: rolls.name, roll: total});
+				$("#join").addClass("hidden");
+				$("#play").removeClass("hidden");
+
+				socket.emit("sync_all");
+			}
+		});
+
+		$("#pc-join").on("click", function(){
+			var name = $("#player-name").val();
+			if(name == ""){
+				toast("Please enter a name.");
+				$("#player-name").addClass("error");
+				$("#player-name").focus();
+			} else {
+				socket.emit("pc_join", {
+					name: $("#player-name").val(),
+					id: fae.id,
+				});
+				clearInterval(gmCheck);
+				$("#join").addClass("hidden");
+				$("#play").removeClass("hidden");
+
+				socket.emit("sync_all");
+			}
+		});
+
+		socket.on("gm_get", function(data){
+			$("#gm-id").text(data.name);
+		});
+
+		socket.on("sync_all", function(data){
+			fae.players = data.players;
+			fae.characters = data.characters;
+		});
+		socket.on("sync_players", function(data){
+			fae.players = data;
+		});
+		socket.on("sync_characters", function(data){
+			fae.characters = data;
+		});
+
+		socket.on("kick", function(data){
+			document.location.reload();
+		});
+	});
+
+	$("#sync-btn").on("click", function(){ socket.emit("sync_all"); });
+	$("#reset-btn").on("click", function(){ socket.emit("reset"); });
+
+	$("#video").on("click", ".close", function(e){
+		var id = $(this).attr("data-id");
+		socket.emit("pc_kick", {id: id});
+		for(var i = 0; i < fae.players.length; i++){
+			if(fae.players[i].id == id){
+				fae.players.splice(i, 1);
+				break;
+			}
+		}
+	});
+
+	$("#save-character").on("click", function(e){
+		$("#mkchar").prop("checked", false);
+		toast("Character saved.");
+		fae.characters.push({});
+		console.log(fae.characters);
+		socket.emit("char_new", {});
+	});
+
+	$(document.body).on("keydown", function(e){
+		if(e.keyCode == 27){ // ESC
+			// close modals
+			console.log("closing modals");
+			$('.modal input[type=checkbox]').each(function(){
+				console.log(this);
+				$(this).prop("checked", false);
+			});
 		}
 	});
 })();
